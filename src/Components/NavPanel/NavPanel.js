2bd561f259d869b07e3b66df58e08da758e14fd4
MarinaBolgova <marinkan30@gmail.com>
2022-05-25 19:35:10 +0300

    первый коммит src

---
+++ src/Components/NavPanel/NavPanel.js
@@ -0,0 +1,18 @@
+
+
+function NavPanel() {
+    return (
+        <div className="nav">
+             <a href=""> Главная страница </a>
+             <a href=""> О нас </a>
+             <a href=""> Контакты </a>
+             <a href=""> Магазин </a>
+        </div>
+
+);
+
+}
+
+export default NavPanel;
+
+
